d8c95ded0f7b34d2954d5b56f3c11bbe8b3c3798
Eric Vieira Pereira <ericvieira.p@outlook.com>
2023-09-20 13:32:58 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "MAIS UM GOL DE CRISTTIANO RONALDO",
+    "description": "Cristiano Ronaldo marca seu gol de número 850 pelo AL NASR",
+    "photoCover": "https://th.bing.com/th?id=OIF.PlnYYjbbWyZ%2b9LRehxAULw&pid=ImgDet&rs=1"
+  },
+
+  {
+    "id":"2",
+    "title": "saiu novo filme",
+    "description": "ola",
+    "photoCover": "ola"
+  }
+]
